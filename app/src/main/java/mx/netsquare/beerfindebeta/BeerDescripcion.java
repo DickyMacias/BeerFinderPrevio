5da4fced4346cece593a21503fb0a47cee4402ef
Dicky Macias <maciasfusco@gmail.com>
2017-08-07 02:02:38 -0600

    Actualizado version presentacion (No mas cambios)

--- app/src/main/java/mx/netsquare/beerfindebeta/BeerDescripcion.java
+++ app/src/main/java/mx/netsquare/beerfindebeta/BeerDescripcion.java
@@ -9,5 +9,6 @@ public class BeerDescripcion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beer_descripcion);
+
     }
 }
